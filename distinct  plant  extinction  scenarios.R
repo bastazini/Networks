8af8754b89b3b48ad315b307b9e917c494e54aed
bastazini <bastazini.vinicius@gmail.com>
2017-07-04 10:11:19 +0200

    Simulation study investigating the effect of distinct extinction scenarios on the robustness of a seed dispersal network. Details of this paper will follow upon publication

---
+++ distinct  plant  extinction  scenarios.R
@@ -0,0 +1,128 @@
+###################################
+#Paper Title pending
+#Bastazini V. A. G., Debastiani, V. J., Guimarães, P. R., & Pillar, V. D. (in prep)
+#Last updated: 2017-06-04
+#Contacts: 
+#bastazini.vinicius@gmail.com
+#vanderleidebastiani@yahoo.com.br
+###################################
+
+####Scenarios of extinction
+
+##Functional distinctiveness
+functional=(hclust(dist(atributos)))
+tree<-compute.brlen(as.phylo(functional))
+plot(tree)
+
+distinc<-evol.distinct(tree,type ="fair.proportion")
+distinc
+
+distinc.order<-distinc[order(distinc[,2], decreasing = TRUE), ]
+distinc.order$Order<-1:length(tree$tip.label)
+distinc.order
+
+rownames(rede)==distinc.order[,1]
+extinct.row<-distinc.order[match(rownames(rede),distinc.order[,1]),3]
+extinct.row
+
+rob.functional=second.extinct(rede, participant="lower", method="external", nrep=50,details=FALSE,ext.row=extinct.row)
+robustness(rob.functional)
+fit.hyperbolica(rob.functional)
+
+
+##Phylogenetic distinctiveness
+tree<-rcoal(9) 
+tree$tip.label=sample(rownames(Safariland))
+filo=(hclust(filo))
+tree<-compute.brlen(as.phylo(filo))
+plot(tree)
+
+distinc<-evol.distinct(tree,type ="fair.proportion")
+distinc
+
+distinc.order<-distinc[order(distinc[,2], decreasing = TRUE), ]
+distinc.order$Order<-1:length(tree$tip.label)
+distinc.order
+
+rownames(rede)==distinc.order[,1]
+extinct.row<-distinc.order[match(rownames(rede),distinc.order[,1]),3]
+extinct.row
+
+rob.filogenetica=second.extinct(rede, participant="lower", method="external", nrep=50,details=FALSE,ext.row=extinct.row)
+robustness(rob.filogenetica)
+fit.hyperbolica(rob.filogenetica)
+
+
+##Random
+rob.aleat=second.extinct(rede, participant="lower", method="random", nrep=1000,details=FALSE,ext.row=extinct.row)
+robustness(rob.aleat)
+fit.hyperbolica(rob.aleat)
+
+##Specialization
+rob.grau=second.extinct(rede, participant="lower", method="degree", nrep=1000,details=FALSE,ext.row=extinct.row)
+robustness(rob.grau)
+fit.hyperbolica(rob.grau)
+#
+rob.abun=second.extinct(rede, participant="lower", method="abun", nrep=1000,details=FALSE,ext.row=extinct.row)
+robustness(rob.abun)
+fit.hyperbolica(rob.abun)
+
+##Grafs
+par(mfrow=c(3,2))
+fit.hyperbolica(rob.aleat)
+legend("topright", legend = "i", cex=1.2, bty = "n")
+fit.hyperbolica(rob.grau)
+legend("topright", legend = "ii", cex=1.2,bty = "n")
+fit.hyperbolica(rob.filogenetica)
+legend("topright", legend = "iii",cex=1.2, bty = "n")
+fit.hyperbolica(rob.funcional)
+legend("topright", legend = "iv", cex=1.2,bty = "n")
+fit.hyperbolica(rob.abun)
+legend("topright", legend = "v", cex=1.2,bty = "n")
+
+
+fit.hyperbolica=function (object, plot.it = TRUE, ...) 
+{
+  if (class(object) != "bipartite") 
+    stop("This function cannot be meaningfully applied to objects of this class.")
+  N <- colSums(object)
+  if (all(object[-nrow(object), 2] == 1)) 
+    y <- -object[, 3]
+  else y <- -object[, 2]
+  y <- (sum(y) - cumsum(y))/sum(y)
+  x <- (object[, "no"]/max(object[, "no"]))
+  fit <- try(nls(y ~ 1 - x^a, start = list(a = 1)))
+  if (class(fit) == "try-error") 
+    fit <- nls((y + rnorm(length(y), s = 0.01)) ~ 1 - x^a, 
+               start = list(a = 1))
+  if (plot.it) {
+    par(mar = c(5, 5, 1, 1))
+    plot(x, y, xlab = "Fraction of elimited plants", 
+         ylab = "Fraction of surviving birds", 
+         axes = TRUE, type = "n", cex.lab = 1)
+    
+    
+    points(x, y, ...)
+    lines(seq(0, 1, 0.1), predict(fit, newdata = data.frame(x = seq(0, 
+                                                                    1, 0.1))), col = "red", lwd = 2)
+  }
+  return(c(exponent = as.numeric(coef(fit)[1])))
+}
+
+##############################
+## Figure 2
+##############################
+
+disti=read.table(file.choose(), h=T)
+layout(matrix(c(1,2),1,2,byrow=T), widths=c(0.4,0.4))
+par(mar=c(5.4,1,1.2,0))
+plot(as.phylo(hclust(vegdist(filo,method="euclidean"))),cex=0.7)
+par(mar=c(5,0,1,2))
+barplot(t(disti), beside = TRUE, horiz=T,axisnames=F, xlim=c(0,0.6), xlab="Distinctiveness",
+        legend = c("Phylogenetic","Functional"), args.legend = list(x="bottomright", bty = "n", ncol = 1,cex=0.8))
+
+par(mar=c(5,3,1,2))
+barplot(t(disti), beside = TRUE, horiz=T,axisnames=T, xlim=c(0,0.6), xlab="Distinctiveness",las=1)
+
+rownames(filo)==rownames(disti)
+
