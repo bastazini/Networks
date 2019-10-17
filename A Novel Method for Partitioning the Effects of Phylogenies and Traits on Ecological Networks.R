a6d055e368b45fde2e54cb97703614df378e9716
bastazini <bastazini.vinicius@gmail.com>
2019-10-17 17:20:37 +0200

    Update A Novel Method for Partitioning the Effects of Phylogenies and Traits on Ecological Networks.R

--- A Novel Method for Partitioning the Effects of Phylogenies and Traits on Ecological Networks.R
+++ A Novel Method for Partitioning the Effects of Phylogenies and Traits on Ecological Networks.R
@@ -1,4 +1,4 @@
-####################################
+###################################
 #R code to reproduce models tested in Table 2 
 #Bastazini, V.A.G., Ferreira, P.M., Azambuja, B.O., Casas, G., Debastiani, V.J., Guimarães, P.R. & Pillar, V.D. 2017. Untangling the Tangled Bank: A Novel Method for Partitioning the Effects of Phylogenies and Traits on Ecological Networks. 
 #Evolutionary Biology  44(3): 312–324. DOI: 10.1007/s11692-017-9409-8
